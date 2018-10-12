d7120a355466f08afa0222fdfb9905939b517648
dahoony <crysis1@naver.com>
2018-10-12 10:51:27 +0900

    Update SQL

--- BLAuction/src/com/bla/sql/blauctionSql.sql
+++ BLAuction/src/com/bla/sql/blauctionSql.sql
@@ -31,12 +31,6 @@ auct_type_id number(5) not null PRIMARY KEY,
 auct_type_name varchar2(10) not null
 );
 
-create table DELIVERY(
-delivery_code varchar2(50) not null PRIMARY KEY,
-delivery_status varchar2(10) not null,
-payment_status varchar2(10) not null
-);
-
 create table AUCTION(
 auct_id number(5) not null PRIMARY KEY,
 member_id number(5) not null,
@@ -50,6 +44,7 @@ down_price number(10) null,
 down_term number(10) null,
 auction_status varchar2(10) default 'before' check(auction_status in ('before','proceeding','end','cancel')),
 auction_address varchar2(160) null,
+tag varchar2(255) null,
 CONSTRAINT FK_AUCTION_member FOREIGN KEY(member_id)
 REFERENCES MEMBER(member_id),
 CONSTRAINT FK_AUCTION_auct_type FOREIGN KEY(auct_type_id)
@@ -58,20 +53,13 @@ CONSTRAINT FK_AUCTION_cate_type FOREIGN KEY(cate_type_id)
 REFERENCES CATEGORY_TYPE(cate_type_id)
 );
 
+//수정 가능성 있는 테이블
 create table PHOTO(
 photo_id number(5) not null PRIMARY KEY,
 photo_name varchar2(20) not null,
 photo_path varchar2(255) not null,
 auct_id number(5) not null,
 CONSTRAINT FK_PHOTO_auct FOREIGN KEY(auct_id)
-REFERENCES AUCTION(auct_id)
-);
-
-create table TAG(
-tag_id number(5) not null PRIMARY KEY,
-tag_name varchar2(50) not null,
-auct_id number(5) not null,
-CONSTRAINT FK_TAG_auct FOREIGN KEY(auct_id)
 REFERENCES AUCTION(auct_id)
 );
 
@@ -91,17 +79,16 @@ REFERENCES AUCTION(auct_id)
 
 create table SUCCESSFUL_BID(
 auct_id   number(5) not null PRIMARY KEY,
-bid_id   number(5) null,
+bid_id   number(5) not null,
 review   varchar2(255) null,
 delivery_code varchar2(50) null,
+delivery_status varchar2(10) null,
+company_code number(2) null,
 CONSTRAINT FK_SUCCESSFUL_BID_bid FOREIGN KEY(bid_id)
 REFERENCES BIDDING(bid_id),
-CONSTRAINT FK_SUCCESSFUL_BID_delivery FOREIGN KEY(delivery_code)
-REFERENCES DELIVERY(delivery_code),
 CONSTRAINT FK_SUCCESSFUL_BID_auct_id FOREIGN KEY(auct_id)
 REFERENCES AUCTION(auct_id)
 );
-
 
 DROP SEQUENCE SEQ_MEMBER;
 CREATE SEQUENCE SEQ_MEMBER
@@ -133,9 +120,9 @@ INSERT INTO CATEGORY_TYPE VALUES(5,'furniture');
 INSERT INTO CATEGORY_TYPE VALUES(6,'etc');
 
 //경매 종류 데이터 삽입
-INSERT INTO AUCTION_TYPE VALUES(7,'up');
-INSERT INTO AUCTION_TYPE VALUES(8,'down');
-INSERT INTO AUCTION_TYPE VALUES(9,'secret');
+INSERT INTO AUCTION_TYPE VALUES(1,'up');
+INSERT INTO AUCTION_TYPE VALUES(2,'down');
+INSERT INTO AUCTION_TYPE VALUES(3,'secret');
 
 //테스트용 회원
 INSERT INTO MEMBER VALUES(SEQ_MEMBER.NEXTVAL,'crysis1@naver.com','1234','최다훈','서울특별시 관악구 봉천동 1523-25','01092557434','931217',0,0,'0x9671652cf6fba11f7576b341b95bff03ad27d581');
